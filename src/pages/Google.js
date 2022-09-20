a8890cc28f309302b8c4d89506b64883803abc93
IsraelIyke <nwangwuisrael@gmail.com>
2022-09-20 23:47:12 +0100

    api

--- src/pages/Google.js
+++ src/pages/Google.js
@@ -4,7 +4,7 @@ import gifs from "../images/search.gif";
 import gray from "../images/gray.gif";
 import { BsSearch } from "react-icons/bs";
 import { MdLightMode } from "react-icons/md";
-import { BsToggleOn, BsToggleOff, BsFillEyeFill } from "react-icons/bs";
+import { BsToggleOn, BsToggleOff } from "react-icons/bs";
 import Textfield from "./Textfield/textfield";
 import "./Textfield/textfield.css";
 
@@ -13,13 +13,13 @@ export default function Google() {
   const [site, setSite] = useState([]);
   const [exact, setExact] = useState();
   const [intensity, setIntensity] = useState(false);
-  const [eyeCare, setEyeCare] = useState(false);
+  const [grayscale, setGrayscale] = useState(false);
 
   function handleIntensity() {
     setIntensity((prev) => !prev);
   }
   function handleEyeCare() {
-    setEyeCare((prev) => !prev);
+    setGrayscale((prev) => !prev);
   }
   function handleSubmit() {
     setSearch((prev) => {
@@ -38,178 +38,191 @@ export default function Google() {
     setSearch("");
   }
   return (
-    <Box flexGrow={1}>
-      <Grid container spacing={1} justifyContent="center">
-        <div
-          className={
-            (eyeCare === false && "container-dark") ||
-            (eyeCare && "container-eye-care")
-          }
-        >
-          <div className="blur"></div>
-          {intensity && (
-            <div className="close-window" onClick={handleIntensity}></div>
-          )}
-          <Grid item>
-            <nav className="sup-container">
-              <ul>
-                <li>
-                  {intensity && (
-                    <div className="intensity-option">
-                      <li className="percent">
-                        <li>0%</li>
-                        <li>50%</li>
-                        <li>100%</li>
-                      </li>
-
-                      <li className="percentage" onClick={handleEyeCare}>
-                        <li>grayscale</li>
-                        {eyeCare ? (
-                          <li>
-                            <BsToggleOn className="eye-icon" />
-                          </li>
-                        ) : (
-                          <li>
-                            <BsToggleOff className="eye-icon" />
-                          </li>
-                        )}
-                      </li>
-                    </div>
-                  )}
-                </li>
-                <li>
-                  <MdLightMode
-                    className="dark-icon"
-                    onClick={handleIntensity}
-                  />
-                </li>
-              </ul>
-            </nav>
-          </Grid>
-          <Grid item>
-            <div className="title">
-              <h2
-                className={eyeCare ? "motion-gray" : "motion"}
-                data-text="Google"
-              >
-                Google
-                <span
-                  style={{
-                    color: "transparent",
-                    backgroundColor: "transparent",
-                  }}
-                >
-                  .
-                </span>
-              </h2>
-              <h2
-                className={eyeCare ? "motion-gray" : "motion"}
-                data-text="Like"
-              >
-                Like
-                <span
-                  style={{
-                    color: "transparent",
-                    backgroundColor: "transparent",
-                  }}
-                >
-                  .
-                </span>
-              </h2>
-              <h2 className={eyeCare ? "motion-gray" : "motion"} data-text="A">
-                A
-                <span
-                  style={{
-                    color: "transparent",
-                    backgroundColor: "transparent",
-                  }}
-                >
-                  .
-                </span>
-              </h2>
-              <h2
-                className={eyeCare ? "motion-gray" : "motion"}
-                data-text="Pro"
-              >
-                Pro
-                <span
-                  style={{
-                    color: "transparent",
-                    backgroundColor: "transparent",
-                  }}
-                >
-                  .
-                </span>
-              </h2>
-            </div>
-          </Grid>
-          <form
-            action="https://google.com/search"
-            onSubmit={handleSubmit}
-            target="_blank"
-          >
+    <div
+      className={
+        (grayscale === false && "container-dark") ||
+        (grayscale && "container-grayscale")
+      }
+    >
+      <Box flexGrow={1}>
+        <Grid container spacing={1} justifyContent="center">
+          <div className="container">
+            <div className="blur"></div>
+            {intensity && (
+              <div className="close-window" onClick={handleIntensity}></div>
+            )}
             <Grid item>
-              <div className="sub-container">
-                <input
-                  type="text"
-                  name="q"
-                  value={search}
-                  onChange={(e) => setSearch(e.target.value)}
-                  onFocus={handleClear}
-                  className="input"
-                  autoComplete="off"
-                />
+              <nav className="sup-container">
+                <ul>
+                  <li>
+                    {intensity && (
+                      <div className="intensity-option">
+                        <li className="percent">
+                          <li>0%</li>
+                          <li>50%</li>
+                          <li>100%</li>
+                        </li>
 
-                <button className="search">
-                  <BsSearch className="search" />
-                </button>
+                        <li className="percentage" onClick={handleEyeCare}>
+                          <li>grayscale</li>
+                          {grayscale ? (
+                            <li>
+                              <BsToggleOn className="eye-icon" />
+                            </li>
+                          ) : (
+                            <li>
+                              <BsToggleOff className="eye-icon" />
+                            </li>
+                          )}
+                        </li>
+                      </div>
+                    )}
+                  </li>
+                  <li>
+                    <MdLightMode
+                      className="dark-icon"
+                      onClick={handleIntensity}
+                    />
+                  </li>
+                </ul>
+              </nav>
+            </Grid>
+            <Grid item>
+              <div className="title">
+                <h2
+                  className={grayscale ? "motion-gray" : "motion"}
+                  data-text="Google"
+                >
+                  Google
+                  <span
+                    style={{
+                      color: "transparent",
+                      backgroundColor: "transparent",
+                    }}
+                  >
+                    .
+                  </span>
+                </h2>
+                <h2
+                  className={grayscale ? "motion-gray" : "motion"}
+                  data-text="Like"
+                >
+                  Like
+                  <span
+                    style={{
+                      color: "transparent",
+                      backgroundColor: "transparent",
+                    }}
+                  >
+                    .
+                  </span>
+                </h2>
+                <h2
+                  className={grayscale ? "motion-gray" : "motion"}
+                  data-text="A"
+                >
+                  A
+                  <span
+                    style={{
+                      color: "transparent",
+                      backgroundColor: "transparent",
+                    }}
+                  >
+                    .
+                  </span>
+                </h2>
+                <h2
+                  className={grayscale ? "motion-gray" : "motion"}
+                  data-text="Pro"
+                >
+                  Pro
+                  <span
+                    style={{
+                      color: "transparent",
+                      backgroundColor: "transparent",
+                    }}
+                  >
+                    .
+                  </span>
+                </h2>
               </div>
             </Grid>
-            <Grid item>
-              <Textfield
-                type="text"
-                placeholder="exact site"
-                id="site"
-                label="exact site"
-                setState={setSite}
-                value={site}
-                autoComplete="off"
-              />
-            </Grid>
-            <Grid item>
-              <input
-                type="checkbox"
-                checked={exact}
-                onChange={(e) => setExact(e.target.checked)}
-                className="inpu"
-              />
-            </Grid>
-          </form>
-          <Grid item>
-            <div>Search parameters</div>
-          </Grid>
 
-          <Grid>
-            {eyeCare ? (
-              <img
-                src={gray}
-                alt="bg"
-                className="img"
-                width={500}
-                height={500}
-              />
-            ) : (
-              <img
-                src={gifs}
-                alt="bg"
-                className="img"
-                width={500}
-                height={500}
-              />
-            )}
-          </Grid>
-        </div>
-      </Grid>
-    </Box>
+            <form
+              action="https://google.com/search"
+              onSubmit={handleSubmit}
+              target="_blank"
+            >
+              <Grid item>
+                <div className="sub-container">
+                  <input
+                    type="text"
+                    name="q"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    onFocus={handleClear}
+                    className="input"
+                    autoComplete="off"
+                  />
+
+                  <button className="search">
+                    <BsSearch className="search" />
+                  </button>
+                </div>
+              </Grid>
+              <Grid item>
+                <div className="param">Search specifiers</div>
+              </Grid>
+              <Grid item>
+                <p className="para">use only relevant specifiers</p>
+              </Grid>
+              <div className="par">
+                <Grid item>
+                  <Textfield
+                    type="text"
+                    placeholder="exact site"
+                    id="site"
+                    label="exact site"
+                    setState={setSite}
+                    value={site}
+                    autoComplete="off"
+                  />
+                </Grid>
+
+                <Grid item className="checks">
+                  <div>exact phrase</div>
+                  <input
+                    type="checkbox"
+                    checked={exact}
+                    onChange={(e) => setExact(e.target.checked)}
+                    className="inpu"
+                  />
+                </Grid>
+              </div>
+            </form>
+
+            <Grid>
+              {grayscale ? (
+                <img
+                  src={gray}
+                  alt="bg"
+                  className="img"
+                  width={500}
+                  height={500}
+                />
+              ) : (
+                <img
+                  src={gifs}
+                  alt="bg"
+                  className="img"
+                  width={500}
+                  height={500}
+                />
+              )}
+            </Grid>
+          </div>
+        </Grid>
+      </Box>
+    </div>
   );
 }
